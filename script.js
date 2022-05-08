688da7da058913f3559787f0f836896d7075be6b
Stephen <stephen.lyssy@gmail.com>
2022-05-08 15:12:32 -0500

    Starting process of converting html to a slide show.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.nav-links');
-const pageNavbar = document.querySelector('.page-navbar');
-const pageLinks = document.querySelector('.page-links');
-const stickyClose = document.querySelector('.close-icon');
+// const pageNavbar = document.querySelector('.page-navbar');
+// const pageLinks = document.querySelector('.page-links');
+// const stickyClose = document.querySelector('.close-icon');
 
 //% Toggle Navbar
 //% Navbar toggle button event listener
@@ -11,29 +11,29 @@ toggleButton.addEventListener('click', (e) => {
   navbarLinks.classList.toggle('active');
 });
 
-//% Sticky Navbar
-window.addEventListener('scroll', () => {
-  pageNavbar.classList.toggle('sticky', window.scrollY > 0);
-});
+// //% Sticky Navbar
+// window.addEventListener('scroll', () => {
+//   pageNavbar.classList.toggle('sticky', window.scrollY > 0);
+// });
 
-stickyClose.addEventListener('click', (e) => {
-  pageNavbar.classList.remove('sticky');
-});
+// stickyClose.addEventListener('click', (e) => {
+//   pageNavbar.classList.remove('sticky');
+// });
 
-//% Smooth Scroll
+// //% Smooth Scroll
 
-const navLinks = document.querySelector('.nav__links');
-//? 1. Add an event listener to common parent element.
-pageLinks.addEventListener('click', function (e) {
-  e.preventDefault();
-  //? 2. Determine what element originated the event.
-  // console.log(e.target);
-  // $ Matching Strategy: This makes it so only the links in the nav__links
-  // $ container are selected and not the nav_links container itself.
-  if (e.target.classList.contains('page-link')) {
-    const pageNavId = e.target.getAttribute('href');
-    console.log(pageNavId);
-    document.querySelector(pageNavId).scrollIntoView({ behavior: 'smooth' });
-    pageNavbar.classList.remove('sticky');
-  }
-});
+// const navLinks = document.querySelector('.nav__links');
+// //? 1. Add an event listener to common parent element.
+// pageLinks.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   //? 2. Determine what element originated the event.
+//   // console.log(e.target);
+//   // $ Matching Strategy: This makes it so only the links in the nav__links
+//   // $ container are selected and not the nav_links container itself.
+//   if (e.target.classList.contains('page-link')) {
+//     const pageNavId = e.target.getAttribute('href');
+//     console.log(pageNavId);
+//     document.querySelector(pageNavId).scrollIntoView({ behavior: 'smooth' });
+//     pageNavbar.classList.remove('sticky');
+//   }
+// });
